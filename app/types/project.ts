0ed2c3b442b364d25632165822ed71ddc08ef618
Nancy <52145579+aLiz-Nancy@users.noreply.github.com>
2022-11-01 13:11:07 +0900

    feat: create project base types

---
+++ app/types/project.ts
@@ -0,0 +1,21 @@
+import { Image } from './image';
+import { Link } from './link';
+
+export type ProjectContent = {
+  id: string;
+  title: string;
+  body: string;
+};
+
+export type Project = {
+  id: string;
+  name: string;
+  thumbnail: Image;
+  meta: {
+    startedAt: number;
+  }
+  body: {
+    contents: ProjectContent[];
+    links: Link[];
+  }
+};
