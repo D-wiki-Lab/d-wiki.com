188440f8413648199f1291b67a45d8c9867c5b37
Nancy <52145579+aLiz-Nancy@users.noreply.github.com>
2022-11-01 13:02:11 +0900

    feat: create user base types

---
+++ app/types/user.ts
@@ -0,0 +1,11 @@
+import { Image } from './image';
+import { Wallet } from './wallet';
+
+export type User = {
+  id: string;
+  name: string;
+  wallet: Wallet[];
+  profileImage?: Omit<Image, 'width' | 'height'>;
+};
+
+export type UserPublic = Omit<User, 'wallet'>;
