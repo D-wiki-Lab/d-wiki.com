b9a619a43cc8e41982ea124301a192e674a84c49
Nancy <52145579+aLiz-Nancy@users.noreply.github.com>
2022-11-04 14:28:12 +0900

    feat(view): add header

---
+++ app/components/Nav/Header/NavHeader.vue
@@ -0,0 +1,88 @@
+<template>
+  <header class="NavHeader">
+    <div class="NavHeader-container">
+      <div class="NavHeader-first">
+        <NuxtLink to="/" class="NavHeader-logo">
+          <IconLogo :width="117" :height="24" />
+        </NuxtLink>
+      </div>
+      <div class="NavHeader-second">
+        <NuxtLink
+          v-if="isLogin"
+          to="/me/"
+          class="NavHeader-wallet -is-me"
+        >
+          <IconUser :width="32" :height="32" />
+        </NuxtLink>
+        <NuxtLink
+          v-else
+          to="/add-wallet/"
+          class="NavHeader-wallet -is-yet"
+        >
+          CONNECT WALLET
+        </NuxtLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+  isLogin: boolean,
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style lang="scss" scoped>
+.NavHeader {
+  $root: &;
+
+  @include flex(null, center);
+  @include box-pd(4px, 20px, 4px, 20px);
+  @include bg(hsla(0, 0%, 100%, .56));
+
+  backdrop-filter: blur(4px);
+
+  > &-container {
+    @include flex(null, center);
+    @include box(100%);
+
+    > #{$root}-first,
+    > #{$root}-second {
+      flex: 1;
+    }
+
+    > #{$root}-first {
+      @include flex(null, center);
+
+      > #{$root}-logo {
+        @include box-pd(12px, 20px, 12px, 20px);
+
+        > .IconLogo {
+          @include box(117px, 24px);
+        }
+      }
+    }
+
+    > #{$root}-second {
+      @include flex(flex-end, center);
+
+      > #{$root}-wallet {
+        &.-is-me {
+          @include box-pd(12px, 16px, 12px, 16px);
+
+          > .IconUser {
+            @include box(32px, 32px);
+          }
+        }
+
+        &.-is-yet {
+          @include box-pd(12px, 20px, 12px, 20px);
+          @include font(null, 700);
+        }
+      }
+    }
+  }
+}
+</style>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,23 @@
 <template>
-  <div>
-    A *default* layout
+  <div class="LayoutDefault">
+    <NavHeader :isLogin="false" />
+
     <slot />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.LayoutDefault {
+  @include box-pd(64px);
+
+  > .NavHeader {
+    @include box(null, 64px);
+
+    position: fixed;
+    top: 0;
+    right: 0;
+    left: 0;
+    z-index: 7000;
+  }
+}
+</style>
